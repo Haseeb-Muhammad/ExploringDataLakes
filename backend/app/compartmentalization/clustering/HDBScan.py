489017e2d58b29d7be5a0a79795a3e362e33926a
Haseeb Muhammad <huhammad.bscs22seecs@seecs.edu.pk>
2025-08-06 10:21:50 +0200

    initilized clustering and compartmentalization with HDBScan

---
+++ backend/app/compartmentalization/clustering/HDBScan.py
@@ -0,0 +1,22 @@
+from clusteror import Clusteror
+import numpy as np
+
+class hdbscan(Clusteror):
+
+    def __init__(self, min_cluster_size:int=2):
+        self.clusterer = hdbscan.HDBSCAN(min_cluster_size=min_cluster_size, metric='euclidean')
+
+    def cluster(embeddings:np.ndarray):
+        """
+        Perform clustering on the input embeddings.
+
+        Args:
+            embeddings (np.ndarray): embeddings of shape [number of tables, embedding dimentions]
+
+        Returns:
+            labels (dict{int : List[int]}): Cluster labels for each input point.
+        """
+        self.clusterer.fit(embeddings)
+        return self.clusterer.labels_
+
+
---
+++ backend/app/compartmentalization/clustering/clusteror.py
@@ -0,0 +1,17 @@
+from abc import ABC
+import numpy as np
+
+def Clusteror(ABC):
+
+    @abstractmethod
+    def cluster(self, embeddings:np.ndarray):
+        """
+        Perform clustering on the input embeddings.
+
+        Args:
+            embeddings (np.ndarray): embeddings of shape [number of tables, embedding dimentions]
+
+        Returns:
+            labels (List[int]): Cluster labels for each input point.
+        """
+        pass
