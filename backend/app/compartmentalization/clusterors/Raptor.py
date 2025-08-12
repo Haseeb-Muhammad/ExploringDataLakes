3ff5e14751a420f14a8e0dd0bff09ad58f321e20
Haseeb Muhammad <huhammad.bscs22seecs@seecs.edu.pk>
2025-08-12 09:53:38 +0200

    raptor initialization

---
+++ backend/app/compartmentalization/clusterors/Raptor.py
@@ -0,0 +1,21 @@
+from .clusteror import Clusteror
+from typing import List
+
+class Raptor(Clusteror):
+    def __init__():
+        pass
+
+    def cluster(
+        self,
+        text : List[str],
+    ) -> dict:
+        """
+        Clusters the given list of text data into groups.
+
+        Args:
+            text (List[str]): A list of strings to be clustered.
+
+        Returns:
+            Return a dict: {level: {clusterNo: [table names]}} 
+        """
+        pass
